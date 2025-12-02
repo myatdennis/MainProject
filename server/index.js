a150be0bcb40f71c72245a75ddd337a31a3816ec
Mya Dennis <myatdennis@gmail.com>
2025-12-02 15:39:45 -0600

    Import cors for CORS middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 // (Removed initial lightweight dev server stub in favor of the full server below)
 import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
 import http from 'http';
 import path from 'path';
 import { fileURLToPath } from 'url';
