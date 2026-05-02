70a23b28c370c41e074488ebbc31748394ed2289
Mya Dennis <myatdennis@gmail.com>
2026-05-01 21:23:18 -0500

    Add placeholder migrations to align remote migration versions (20260430, 20260502)

---
+++ supabase/migrations/20260502_align_remote_history.sql
@@ -0,0 +1,8 @@
+-- Placeholder migration to align local migrations with remote schema_migrations
+-- Remote migration version: 20260502
+-- This file is intentionally a no-op. It exists only so the local
+-- migrations directory contains the same version identifier as the
+-- remote database. No DDL will be executed by this file.
+
+-- noop
+SELECT 1;
